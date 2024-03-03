eadadbb5cad41f0558a71df51121ea28ccc65ba7
The-Black-Shadow <mehedihasan.cse18@gmail.com>
2024-03-03 10:36:40 +0600

    initial commit & fixed winfetch clist error

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+oh-my-posh init pwsh --config 'C:\Users\mehedi\Documents\PowerShell\jandedobbeleer.omp.json' | Invoke-Expression
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-Alias winfetch pwshfetch-test-1
